1cef6957adfb87bf5d206d2022aea93b4ba5e9b5
Shani <shanishalel89@gmail.com>
2020-07-25 21:44:00 +0300

    ready

--- Demo.cpp
+++ Demo.cpp
@@ -23,10 +23,10 @@ struct lessThan3 {
 };
 
 int main(int argc, const char * argv[]) {
-
+    
     cout << "####  range:  ####";
-    for (int i: range(5,9))
-        cout << i << " ";      // 5 6 7 8
+	for (int i: range(5,9))
+		cout << i << " ";      // 5 6 7 8
 
     vector<int> vecInt = {1,2,3,4};
     vector<string> vecString = {"Hello", "Bye", "Adam"};
@@ -34,8 +34,8 @@ int main(int argc, const char * argv[]) {
 
     cout << "####  accumulate:  ####";
     cout << endl << "accumulate of range: " << endl;
-    for (int i: accumulate(range(5,9)))
-        cout << i << " ";      // 5 11 18 26
+	for (int i: accumulate(range(5,9)))
+		cout << i << " ";      // 5 11 18 26
 
     cout << endl << "accumulate of vector<string>: " << endl;
     for (auto i: accumulate(vecString) )
@@ -43,8 +43,8 @@ int main(int argc, const char * argv[]) {
     cout << endl << endl;
 
     cout << endl << "accumulate of range with binary operator: " << endl;
-    for (int i: accumulate(range(5,9), [](int x, int y){return x*y;}))
-        cout << i << " ";      // 5 30 210 1680
+	for (int i: accumulate(range(5,9), [](int x, int y){return x*y;}))
+		cout << i << " ";      // 5 30 210 1680
 
     cout << "####  Filter False:  ####";
     cout << endl << "Filter out all numbers less than 3 in vector{1,2,3,4}: " << endl;
